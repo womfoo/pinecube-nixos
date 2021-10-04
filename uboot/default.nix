b49537b61d41fc47bf5b7a026ef77c191ccb80f2
Kranium Gikos Mendoza <kranium@gikos.net>
2021-10-04 14:23:11 +1100

    uboot: use version from 20.09

--- uboot/default.nix
+++ uboot/default.nix
@@ -1,6 +1,14 @@
-{ pkgs }:
+{ pkgs, fetchurl }:
 
-pkgs.buildUBoot {
+pkgs.buildUBoot rec {
+
+  version = "2020.10";
+
+  src = fetchurl {
+    url = "ftp://ftp.denx.de/pub/u-boot/u-boot-${version}.tar.bz2";
+    sha256 = "08m6f1bh4pdcqbxf983qdb66ccd5vak5cbzc114yf3jwq2yinj0d";
+  };
+
   patches = [ ./Pine64-PineCube-uboot-support.patch ];
 
   defconfig = "pinecube_defconfig";
